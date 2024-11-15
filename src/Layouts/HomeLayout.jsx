2c0f1995dea24eba614e16a9bdac0b895f37705f
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-11-15 21:02:42 +0600

    52-7

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import LeftNavbar from "../components/layout-components/LeftNavbar";
 import Navbar from "../components/Navbar";
 
 
@@ -23,7 +24,9 @@ const HomeLayout = () => {
 
 			{/* main section container here  */}
 			<main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12">
-				<aside className="col-span-3">Left Navbar</aside>
+				<aside className="col-span-3">
+					<LeftNavbar></LeftNavbar>
+				</aside>
 				<section className="col-span-6">Main content</section>
 				<aside className="col-span-3">Right Navbar</aside>
 			</main>
