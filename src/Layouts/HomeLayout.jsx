e18c3ae869b545caa5f6e58d2d30a79fa82c08d4
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-11-15 16:46:29 +0600

    52-5

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,12 +1,20 @@
 import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
 
 
 const HomeLayout = () => {
 	return (
 		<div>
+			{/* header section  */}
 			<header>
 				<Header></Header>
 			</header>
+
+			{/* latest news container  */}
+			<section className="w-11/12 mx-auto">
+				<LatestNews></LatestNews>
+			</section>
+
 			<nav></nav>
 			<main></main>
 		</div>
