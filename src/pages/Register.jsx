e50349d22c70d893f021c72b4f38f507a84247a4
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-11-16 14:09:39 +0600

    52-5-4

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,25 +1,55 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
 
 
 const Register = () => {
+
+	const { createNewUser, setUser } = useContext(AuthContext);
+
+	const handleSubmit = e =>{
+		e.preventDefault();
+
+		//get form data 
+		const form = new FormData(e.target);
+		const name = form.get('name')
+		const photo = form.get('photo')
+		const email = form.get('email')
+		const password = form.get('password')
+
+		console.log(name, photo, email, password);
+
+		createNewUser(email, password)
+			.then(result =>{
+				const user = result.user;
+				setUser(user);
+				console.log(user);
+			})
+			.catch((error) =>{
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				console.log(errorCode, errorMessage);
+			})
+	}
+
 	return (
 		<div className="min-h-screen flex justify-center items-center">
 			<div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl rounded-none p-10">
 				<h2 className="text-2xl font-semibold text-center">Register your account</h2>
-				<form className="card-body">
+				<form onSubmit={handleSubmit} className="card-body">
 					
 					<div className="form-control">
 						<label className="label">
 							<span className="label-text">Name</span>
 						</label>
-						<input type="text" placeholder="name" className="input input-bordered" required />
+						<input name="name" type="text" placeholder="name" className="input input-bordered" required />
 					</div>
 
 					<div className="form-control">
 						<label className="label">
 							<span className="label-text">Photo url</span>
 						</label>
-						<input type="text" placeholder="photo-url" className="input input-bordered" required />
+						<input name="photo" type="text" placeholder="photo-url" className="input input-bordered" required />
 					</div>
 
 					{/* email container  */}
@@ -27,14 +57,14 @@ const Register = () => {
 					<label className="label">
 						<span className="label-text">Email</span>
 					</label>
-					<input type="email" placeholder="email" className="input input-bordered" required />
+					<input name="email" type="email" placeholder="email" className="input input-bordered" required />
 					</div>
 
 					<div className="form-control">
 					<label className="label">
 						<span className="label-text">Password</span>
 					</label>
-					<input type="password" placeholder="password" className="input input-bordered" required />
+					<input name="password" type="password" placeholder="password" className="input input-bordered" required />
 					<label className="label">
 						<a href="#" className="label-text-alt link link-hover">Forgot password?</a>
 					</label>
