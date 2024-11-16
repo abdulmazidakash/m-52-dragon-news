e38df36a9195e0779fc54896f88a76b497a93c6f
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-11-16 14:57:15 +0600

    52-5-5

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from "../provider/AuthProvider";
 
 const Navbar = () => {
 
-	const { user } = useContext(AuthContext);
+	const { user, logOut } = useContext(AuthContext);
 
 	return (
 		<div className="flex justify-between items-center">
@@ -19,7 +19,13 @@ const Navbar = () => {
 				<div className="">
 					<img src={userIcon} alt="" />
 				</div>
-				<Link to='/auth/login' className="btn btn-neutral rounded-none">Login</Link>
+				{
+					user && user?.email ? (
+						<button onClick={logOut} className="btn btn-neutral rounded-none">Log-Out</button>
+					): (
+						<Link to='/auth/login' className="btn btn-neutral rounded-none">Login</Link>
+					)
+				}
 			</div>
 		</div>
 	);
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 
@@ -16,14 +16,24 @@ const AuthProvider = ({ children}) => {
 		return createUserWithEmailAndPassword(auth, email, password)
 	}
 
+	const userLogin = (email, password) =>{
+		return signInWithEmailAndPassword(auth, email, password);
+	}
+
+	const logOut = () =>{
+		return signOut(auth);
+	}
+
 	const authInfo = {
 		user,
 		setUser,
 		createNewUser,
+		logOut,
+		userLogin,
 	}
 
 	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+		const unsubscribe = onAuthStateChanged(auth, (currentUser) =>{
 			setUser(currentUser);
 		});
 		return () => {
